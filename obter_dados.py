72dbd99bf632b0a2c36541f733b030b0eb95549e
Daniel Fiuza <dfmoraes@educar.sfiec.org.br>
2021-10-08 09:53:19 -0300

    verifica distancia de coordenadas

--- obter_dados.py
+++ obter_dados.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 import sys
+import platform as pl
+from geopy.distance import great_circle
+
+# coords_1 = (-3.944121,-38.71891)
+# coords_2 = (-3.944121,-38.718906)
+# print(great_circle(coords_1, coords_2).m)
 
 # pesos = np.array([850,600,750,1050,1150,800])
 # conchada = lambda x:x[np.random.choice(x.shape[0],1, replace=False)][0]
@@ -13,31 +19,39 @@ def conchada():
     return pesos[np.random.choice(pesos.shape[0],1, replace=False)][0]
 
 
-file = 'logs_caminhao_18_08_21.csv'
-df = pd.read_csv(f"dados/{file}", encoding='iso-8859-1')
-df.columns = ["latitude", "longitude", "status", "angle", "fuel", "weight", "velocity", "imei", "date_created"]
-gps = df.loc[df['latitude'] < -1, ['latitude','longitude']].values
-# df_filtered = df.loc[df['latitude'] < -1, :]
-# df_filtered.to_csv('logs_caminhao_18_08_21_filtered.csv')
-# print(df_filtered)
-# # sys.exit()
-gps_data = tuple(zip(df['latitude'].values, df['longitude'].values))
+class dataObject():
+    def __init__(self, name):
+        sep = '\\' if pl.uname().system == 'Windows' else '/'
+        self.df = pd.read_csv(f"dados{sep}{name}", encoding='iso-8859-1')
+        self.df.columns = ["latitude", "longitude", "status", "angle", "fuel", "weight", "velocity", "imei", "date_created"]
+        self.gps = self.df.loc[self.df['latitude'] < -1, ['latitude','longitude']].values
+        self.df_filtered = self.df.loc[self.df['latitude'] < -1]
+        self.gps_data = tuple(zip(self.df['latitude'].values, self.df['longitude'].values))
 
-file2 = 'logs_escavadeira_18_08_21.csv'
-df2 = pd.read_csv(f"dados/{file2}", encoding='iso-8859-1')
-df2.columns = ["latitude", "longitude", "status", "angle", "fuel", "weight", "velocity", "imei", "date_created"]
-gps2 = df2.loc[df2['latitude'] < -1, ['latitude','longitude']].values
-gps_data2 = tuple(zip(df2['latitude'].values, df2['longitude'].values))
-print(gps)
+    def saveDataFrame(self, name_file):
+        self.df_filtered.to_csv(name_file)
 
 
-# sys.exit()
+
+file_cam = 'logs_caminhao_18_08_21.csv'
+file_esc = 'logs_escavadeira_18_08_21.csv'
+cam = dataObject(file_cam)
+esc = dataObject(file_esc)
+
+esc_fil = esc.df_filtered.loc[esc.df_filtered['date_created'] > '2021-08-17 23:59:59']
+esc_gps_fil = esc_fil.loc[:,['latitude','longitude']].values
+
+for i in range(len(esc_gps_fil)):
+    if great_circle(cam.gps[13],esc_gps_fil[i]).m < 5:
+        print(f'coo_cam: {cam.gps[13]}, coo_esc: {esc_gps_fil[i]}, idx_esc: {i}, dist: {great_circle(cam.gps[13],esc_gps_fil[i]).m}')
+
+sys.exit()
 
 
 fig = go.Figure(go.Scattermapbox(
     mode = "markers+lines",
-    lon = gps[:,1],
-    lat = gps[:,0],
+    lon = cam.gps[:,1],
+    lat = cam.gps[:,0],
     text = 'caminhao',
     marker = {'size': 10,
               'color': 'blue'}))
@@ -45,8 +59,8 @@ fig = go.Figure(go.Scattermapbox(
 
 fig.add_trace(go.Scattermapbox(
     mode = "markers+lines",
-    lon = gps2[:,1],
-    lat = gps2[:,0],
+    lon = esc.gps[:,1],
+    lat = esc.gps[:,0],
     text = 'escavadeira',
     marker = {'size': 10,
               'color': 'red'}))
